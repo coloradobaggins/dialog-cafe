d0d8503ab4c25379bc9f3144dd43ed761ff25cda
coloradobaggins <bugio89@gmail.com>
2022-09-19 16:29:46 -0300

    json, urlencoded

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/test', (req, res)=>{
     res.status(200).json({msg: 'all ok'});
 })
